19f6f3ea975fac9964975cd9b9872944ae621a92
Shevchik <Shev4ik.den@gmail.com>
2015-06-28 15:40:36 +0400

    Add api to get protocolversion from ProxiedPlayer, bump version.

--- src/protocolsupport/api/ProtocolSupportAPI.java
+++ src/protocolsupport/api/ProtocolSupportAPI.java
@@ -2,9 +2,14 @@ package protocolsupport.api;
 
 import java.net.SocketAddress;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import protocolsupport.protocol.storage.ProtocolStorage;
 
 public class ProtocolSupportAPI {
+
+	public static ProtocolVersion getProtocolVersion(ProxiedPlayer player) {
+		return getProtocolVersion(player.getAddress());
+	}
 
 	public static ProtocolVersion getProtocolVersion(SocketAddress address) {
 		return ProtocolStorage.getProtocolVersion(address);
