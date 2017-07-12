6f88fa3ce67ca87adcdda2f46ef6562a6bd50f99
Shevchik <Shev4ik.den@gmail.com>
2017-07-12 10:35:17 +0300

    Some more commands

--- src/protocolsupport/CommandHandler.java
+++ src/protocolsupport/CommandHandler.java
@@ -1,8 +1,16 @@
 package protocolsupport;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
+import protocolsupport.api.Connection;
+import protocolsupport.api.ProtocolSupportAPI;
+import protocolsupport.api.ProtocolVersion;
 
 public class CommandHandler extends Command {
 
@@ -12,13 +20,33 @@ public class CommandHandler extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if ((args.length == 1) && args[0].equals("debug")) {
-			if (LoggerUtil.isEnabled()) {
-				sender.sendMessage(new TextComponent("Disabled logger"));
-				LoggerUtil.setEnabled(false);
-			} else {
-				LoggerUtil.setEnabled(true);
-				sender.sendMessage(new TextComponent("Enabled logger"));
+		if (args.length == 1 || args.length == 2) {
+			if (args[0].equals("debug")) {
+				if (LoggerUtil.isEnabled()) {
+					sender.sendMessage(new TextComponent("Disabled logger"));
+					LoggerUtil.setEnabled(false);
+				} else {
+					LoggerUtil.setEnabled(true);
+					sender.sendMessage(new TextComponent("Enabled logger"));
+				}
+			} else if (args[0].equalsIgnoreCase("list")) {
+				sender.sendMessage(new TextComponent(ChatColor.GREEN + "ProtocolSupport Players:"));
+				for (ProtocolVersion version : ProtocolVersion.getAllSupported()) {
+					List<String> players = ProxyServer.getInstance().getPlayers().stream()
+					.filter(player -> ProtocolSupportAPI.getProtocolVersion(player) == version)
+					.map(player -> player.getName())
+					.collect(Collectors.toList());
+					if (!players.isEmpty() || (args.length == 2 && (args[1].equalsIgnoreCase("v") || args[1].equalsIgnoreCase("verbose")))) {
+						sender.sendMessage(new TextComponent(ChatColor.GOLD + "[" + version.getName() + "]: " + ChatColor.GREEN + String.join(", ", players)));
+					}
+				}
+				if (args.length == 1 || !(args[1].equalsIgnoreCase("v") || args[1].equalsIgnoreCase("verbose"))) {
+					sender.sendMessage(new TextComponent(ChatColor.GOLD + "List all compatible versions using " + ChatColor.GREEN + "/psb list verbose"));
+				}
+			} else if (args[0].equalsIgnoreCase("connections")) {
+				for (Connection connection : ProtocolSupportAPI.getConnections()) {
+					sender.sendMessage(new TextComponent(ChatColor.GREEN + connection.toString()));
+				}
 			}
 		}
 	}
