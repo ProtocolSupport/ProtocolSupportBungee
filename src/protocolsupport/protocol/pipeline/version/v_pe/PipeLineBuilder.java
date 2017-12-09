979f12c10e3670f31cae33ce0a1df89e8aef9c0e
Shevchik <Shev4ik.den@gmail.com>
2017-12-09 10:30:37 +0300

    Add keepalives to bungee->server pe connection

--- src/protocolsupport/protocol/pipeline/version/v_pe/PipeLineBuilder.java
+++ src/protocolsupport/protocol/pipeline/version/v_pe/PipeLineBuilder.java
@@ -1,9 +1,14 @@
 package protocolsupport.protocol.pipeline.version.v_pe;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelPipeline;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.connection.DownstreamBridge;
 import net.md_5.bungee.connection.UpstreamBridge;
@@ -15,6 +20,7 @@ import protocolsupport.protocol.packet.handler.PEEntityRewriteUpstreamBridge;
 import protocolsupport.protocol.pipeline.IPipeLineBuilder;
 import protocolsupport.protocol.pipeline.common.PacketCompressor;
 import protocolsupport.protocol.pipeline.common.PacketDecompressor;
+import protocolsupport.protocol.serializer.VarNumberSerializer;
 import protocolsupport.protocol.storage.NetworkDataCache;
 import protocolsupport.protocol.utils.EncapsulatedProtocolUtils;
 import protocolsupport.utils.ReflectionUtils;
@@ -52,6 +58,19 @@ public class PipeLineBuilder extends IPipeLineBuilder {
 			}
 		});
 		NetworkDataCache cache = new NetworkDataCache();
+		pipeline.addFirst("idlestatehandler", new IdleStateHandler(0, 5, 0));
+		pipeline.addAfter("idlestatehandler", "keepalive", new ChannelInboundHandlerAdapter() {
+			@Override
+			public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+				if (evt instanceof IdleStateEvent) {
+					if (((IdleStateEvent) evt).state() == IdleState.WRITER_IDLE) {
+						ByteBuf rawemptyframedpacket = Unpooled.buffer();
+						VarNumberSerializer.writeVarInt(rawemptyframedpacket, 0);
+						ctx.writeAndFlush(rawemptyframedpacket);
+					}
+				}
+			}
+		});
 		pipeline.replace(PipelineUtils.PACKET_DECODER, PipelineUtils.PACKET_DECODER, new FromServerPacketDecoder(connection, cache));
 		pipeline.replace(PipelineUtils.PACKET_ENCODER, PipelineUtils.PACKET_ENCODER, new ToServerPacketEncoder(connection, cache));
 		pipeline.addAfter(PipelineUtils.FRAME_PREPENDER, "compress", new PacketCompressor(256));
