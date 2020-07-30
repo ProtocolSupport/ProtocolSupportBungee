6f7b9d22f8e718246164685b5e0d3fd5829c3d40
Shevchik <Shev4ik.den@gmail.com>
2020-07-30 23:29:27 +0300

    Remove remnants of pe support

--- src/protocolsupport/protocol/storage/NetworkDataCache.java
+++ src/protocolsupport/protocol/storage/NetworkDataCache.java
@@ -1,9 +1,5 @@
 package protocolsupport.protocol.storage;
 
-import java.util.UUID;
-
-import org.apache.commons.lang3.Validate;
-
 import net.md_5.bungee.protocol.packet.Handshake;
 import protocolsupport.api.Connection;
 
@@ -27,88 +23,6 @@ public class NetworkDataCache {
 
 	public Handshake getServerHandshake() {
 		return serverHandshake;
-	}
-
-	private byte[] startGameData;
-
-	public byte[] getStartGameData() {
-		return startGameData;
-	}
-
-	public void setStartGameData(byte[] startGameData) {
-		this.startGameData = startGameData;
-	}
-
-	protected String locale = "en_us";
-
-	public void setLocale(String locale) {
-		Validate.notNull(locale, "Client locale can't be null");
-		this.locale = locale.toLowerCase();
-	}
-
-	public String getLocale() {
-		return locale;
-	}
-
-	private UUID peClientUUID;
-
-	public void setPEClientUUID(UUID uuid) {
-		Validate.notNull(uuid, "PE client uuid (identity) can't be null");
-		this.peClientUUID = uuid;
-	}
-
-	public UUID getPEClientUUID() {
-		return peClientUUID;
-	}
-
-	private boolean stashingClientPackets = false;
-
-	public boolean isStashingClientPackets() {
-		return stashingClientPackets;
-	}
-
-	public void setStashingClientPackets(boolean stashingClientPackets) {
-		this.stashingClientPackets = stashingClientPackets;
-	}
-
-	private String skinData;
-
-	public String getSkinData() {
-		return skinData;
-	}
-
-	public void setSkinData(String skinData) {
-		this.skinData = skinData;
-	}
-
-	private String skinGeometry;
-
-	public String getSkinGeometry() {
-		return skinGeometry;
-	}
-
-	public void setSkinGeometry(String skinGeometry) {
-		this.skinGeometry = skinGeometry;
-	}
-
-	private float x, y, z;
-
-	public float getX() {
-		return x;
-	}
-
-	public float getY() {
-		return y;
-	}
-
-	public float getZ() {
-		return z;
-	}
-
-	public void setSpawnLocation(float x, float y, float z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
 	}
 
 }
